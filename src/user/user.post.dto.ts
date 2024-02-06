3217b6207e15e45d48f7d3a2863d23d71590587d
KantaHasegawa <gyoza20000@gmail.com>
2024-02-07 02:54:27 +0900

    add validator

--- src/user/user.post.dto.ts
+++ src/user/user.post.dto.ts
@@ -1,4 +1,10 @@
+import { IsAlphanumeric, Length } from 'class-validator';
+
 export class UserPostDto {
+  @Length(1, 10)
   name: string;
+
+  @IsAlphanumeric()
+  @Length(5, 10)
   password: string;
 }
