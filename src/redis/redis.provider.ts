5030d539b83f9bc488fdfba586ffe522833013d6
KantaHasegawa <gyoza20000@gmail.com>
2024-02-08 01:56:44 +0900

    redis config

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -1,9 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { createClient } from 'redis';
 
 export const RedisProvider = {
   provide: 'REDIS',
-  useFactory: async () => {
-    const client = createClient();
+  inject: [ConfigService],
+  useFactory: async (config: ConfigService) => {
+    const client = createClient({
+      url: `redis://${config.get<string>('REDIS_USERNAME')}:${config.get<string>('REDIS_PASSWORD')}@${config.get<string>('REDIS_HOST')}:6379`,
+    });
     client.on('error', (err) => console.log('Redis Client Error', err));
     await client.connect();
     return client;
